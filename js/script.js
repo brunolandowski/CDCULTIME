14c3eb202f0492742cccdfe3e4ec090c7275df51
brunolandowski <bruno.landowski@gmail.Com>
2017-01-23 15:42:46 +0100

    second

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
 
  $.getJSON('json/lang_fr.json', function (data) {
 
-   $('#hd_bd_top p:nth-child(1)').html(data.homepage.top_left);
-    $('#hd_bd_top p:nth-child(2)').html(data.homepage.top_middle);
+   $('#hd_bd_top p:first-of-type').html(data.homepage.top_left);
+    $('#hd_bd_top p:last-of-type').html(data.homepage.top_middle);
   
  	  $('#hd_title .hd_title_ct h1').html(data.homepage.main_title);
     $('#hd_title .hd_title_ct p').html(data.homepage.headline);
@@ -18,8 +18,8 @@
 
 $("button#fr").click(function(){
     $.getJSON("json/lang_fr.json", function(result_fr){
-      $('#hd_bd_top p:nth-child(1)').html(result_fr.homepage.top_left);
-    $('#hd_bd_top p:nth-child(2)').html(result_fr.homepage.top_middle);
+      $('#hd_bd_top p:first-of-type').html(result_fr.homepage.top_left);
+    $('#hd_bd_top p:last-of-type').html(result_fr.homepage.top_middle);
 
       $('#hd_title .hd_title_ct h1').html(result_fr.homepage.main_title);
       $('#hd_title .hd_title_ct p').html(result_fr.homepage.headline);
@@ -31,8 +31,8 @@ $("button#fr").click(function(){
 
 $("button#en").click(function(){
     $.getJSON("json/lang_en.json", function(result_en){
-       $('#hd_bd_top p:nth-child(1)').html(result_en.homepage.top_left);
-    $('#hd_bd_top p:nth-child(2)').html(result_en.homepage.top_middle);
+       $('#hd_bd_top p:first-of-type').html(result_en.homepage.top_left);
+    $('#hd_bd_top p:last-of-type').html(result_en.homepage.top_middle);
 
       $('#hd_title .hd_title_ct h1').html(result_en.homepage.main_title);
       $('#hd_title .hd_title_ct p').html(result_en.homepage.headline);
