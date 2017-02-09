40fe3ec26a0e34638dd0e0825a85b2d9ef4fd926
brunolandowski <bruno.landowski@gmail.Com>
2017-02-09 15:23:07 +0100

    9fev 15h30

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ function jsonload(x) {
         // region
         $('.users .left p').html(data.aside.users.title);
 
-        $('.description .left p').html(data.aside.description);
+        
         $('.founders .left p').html(data.aside.founders);
         $('.creation .left p').html(data.aside.creation);
         $('.employees .left p').html(data.aside.employees);
@@ -258,7 +258,7 @@ var template = function(content, params) {
  */
 
 var render = function(item) {
-    var panel = '<div data-employees="{{employeesref}}" data-creation="{{creation}}" class="grid-item {{filters}}"><div class="vi_bd"></div><article><h4>{{name}}</h4><span>{{web}}</span><img src=""><div class="vi_cate">{{serviceslab}}</div></article><div class="whenopen"><span class="item_close"><img src="img/cross_black.svg"></span><div class="row description"><div class="left"><p>Description</p></div><div class="right"><p><div class="fr">{{description_fr}}</div><div class="en">{{description_en}}</div></p></div></div><div class="row services"><div class="left"><p>Services</p></div><div class="right">{{serviceslab}}</div></div><div class="row users"><div class="left"><p>Utilisateurs</p></div><div class="right">{{userslab}}</div></div><div class="row clients"><div class="left"><p>Clients</p></div><div class="right">{{clientslab}}</div></div><div class="row technologies"><div class="left"><p>Technologies</p></div><div class="right">{{techlab}}</div></div><div class="row founders"><div class="left"><p>Foundateurs</p></div><div class="right"><p>{{founders}}</p></div></div><div class="row creation"><div class="left"><p>Date de création</p></div><div class="right"><p>{{creation}}</p></div></div><div class="row employees"><div class="left"><p>Nombre de collaborateurs</p></div><div class="right"><p>{{employees}}</p></div></div><div class="row based"><div class="left"><p>Basée en</p></div><div class="right"><p class="{{region}}">{{region}}</p></div></div><span class="item_dropdown"><span class="cale"></span><span>Partager cette startup<img class="arrow" src="img/arrow_up.svg"><div class="item_dropdown_content"><p><a href="http://google.fr">Sur Twitter</a></p><p><a href="http://google.fr">Sur Facebook</a></p><p><a href="http://google.fr">Copier le lien</a></p></div></span></div></div>';
+    var panel = '<div data-employees="{{employeesref}}" data-creation="{{creation}}" class="grid-item {{filters}}"><div class="vi_bd"></div><article><h4>{{name}}</h4><span><a target="_blank" href="{{web}}">{{web}}</a></span><div class="vi_cate">{{serviceslab}}</div></article><div class="whenopen"><span class="item_close"><img src="img/cross_black.svg"></span><img class="logostartup" src="img/logos/{{logo}}.jpg"><div class="row description"><div class="left"><p></p></div><div class="right"><p><div class="fr">{{description_fr}}</div><div class="en">{{description_en}}</div></p></div></div><div class="row services"><div class="left"><p></p></div><div class="right">{{serviceslab}}</div></div><div class="row users"><div class="left"><p>Utilisateurs</p></div><div class="right">{{userslab}}</div></div><div class="row clients"><div class="left"><p>Clients</p></div><div class="right">{{clientslab}}</div></div><div class="row technologies"><div class="left"><p>Technologies</p></div><div class="right">{{techlab}}</div></div><div class="row founders"><div class="left"><p>Foundateurs</p></div><div class="right"><p>{{founders}}</p></div></div><div class="row creation"><div class="left"><p>Date de création</p></div><div class="right"><p>{{creation}}</p></div></div><div class="row employees"><div class="left"><p>Nombre de collaborateurs</p></div><div class="right"><p>{{employees}}</p></div></div><div class="row based"><div class="left"><p>Basée en</p></div><div class="right"><p class="{{region}}">{{region}}</p></div></div><span class="item_dropdown"><span class="cale"></span><span>Partager cette startup<img class="arrow" src="img/arrow_up.svg"><div class="item_dropdown_content"><p><a href="http://google.fr">Sur Twitter</a></p><p><a href="http://google.fr">Sur Facebook</a></p><p><a href="http://google.fr">Copier le lien</a></p></div></span></div></div>';
     return template(panel, item);
 }
 
@@ -368,7 +368,7 @@ getData().then(function(data) {
             creation: item.gsx$creation.$t,
             employees: item.gsx$employees.$t,
             employeesref: item.gsx$employeesref.$t,
-            logo: item.gsx$logo.$t,
+            logo: item.gsx$logorenamed.$t,
             region: item.gsx$region.$t,
 
             // Services
@@ -401,6 +401,7 @@ getData().then(function(data) {
     var filterValueR;
     // init Isotope
     var $grid = $('#wrap').isotope({
+        
         itemSelector: '.grid-item',
         masonry: {
             gutter: 20
@@ -453,7 +454,7 @@ getData().then(function(data) {
         filterValue = filtersisotope.join('');
         console.log(filterValue);
         $grid.isotope();
-        if_zero();
+      
     });
 
     // helper function, remove obj from ary
@@ -485,7 +486,7 @@ getData().then(function(data) {
         filterValueR = filtersisotopeR.join('');
         console.log(filterValueR);
         $grid.isotope();
-        if_zero();
+       
     });
 
     // helper function, remove obj from ary
@@ -502,6 +503,7 @@ getData().then(function(data) {
         qsRegex = new RegExp($quicksearch.val(), 'gi');
         console.log("Search input", qsRegex);
         $grid.isotope();
+
     }));
 
     // ------------- Sort FUNCTION -------------//
@@ -517,6 +519,7 @@ getData().then(function(data) {
         $grid.isotope({
             sortBy: sortValue,
         });
+          if_zero();
 
     });
 
